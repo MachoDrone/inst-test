865ad6cb4614553ae3eb0c3430fa7726a8c6b7e5
MachoDrone <144707337+MachoDrone@users.noreply.github.com>
2024-07-06 18:09:15 -0400

    Create inst4.sh

---
+++ inst4.sh
@@ -0,0 +1,14 @@
+sudo lshw -c display
+prime-select query
+#sudo prime-select intel
+sudo prime-select nvidia
+curl -fsSL https://nvidia.github.io/libnvidia-container/gpgkey | sudo gpg --dearmor -o /usr/share/keyrings/nvidia-container-toolkit-keyring.gpg \
+  && curl -s -L https://nvidia.github.io/libnvidia-container/stable/deb/nvidia-container-toolkit.list | \
+    sed 's#deb https://#deb [signed-by=/usr/share/keyrings/nvidia-container-toolkit-keyring.gpg] https://#g' | \
+    sudo tee /etc/apt/sources.list.d/nvidia-container-toolkit.list \
+  && \
+    sudo apt-get update
+sudo apt-get install -y nvidia-container-toolkit
+sudo nvidia-ctk runtime configure --runtime=docker
+sudo systemctl restart docker
+#bash <(wget -qO- https://nosana.io/register.sh)
