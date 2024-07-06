47eba71b97a132c337dcb5aaf0794702b43845a4
MachoDrone <144707337+MachoDrone@users.noreply.github.com>
2024-07-06 18:08:42 -0400

    Create inst3.sh

---
+++ inst3.sh
@@ -0,0 +1,10 @@
+groups
+sudo usermod -aG docker md
+docker
+docker info
+# SEE https://www.digitalocean.com/community/tutorials/how-to-install-and-use-docker-on-ubuntu-22-04
+sudo lshw -c display
+sudo lshw -c video
+sudo ubuntu-drivers devices
+sudo ubuntu-drivers autoinstall
+sudo shutdown -r now
